8ebc2693329e3ca3d75caa7a22ed797a88ffa68d
Jing <moonlikestone@gmail.com>
2020-12-24 17:48:44 +0800

    update some detail.

--- src/Access/IFCUpdate.cs
+++ src/Access/IFCUpdate.cs
@@ -8,18 +8,40 @@ using IFCLite.Data;
 
 namespace IFCLite.Access
 {
+    /// <summary>
+    /// 處理IFCDatabase更新事項
+    /// </summary>
     public class IFCUpdate
     {
         private IFCDatabase Database { get; set; }
+        /// <summary>
+        /// 新建一個Update物件
+        /// </summary>
+        /// <param name="Database"></param>
         public IFCUpdate(IFCDatabase Database) { this.Database = Database; }
+        /// <summary>
+        /// 更新單一IFC物件
+        /// </summary>
+        /// <param name="ifcObject"></param>
+        /// <returns></returns>
         public bool Object(IFCObject ifcObject)
         {
             return Database.IFCModel.Update(ifcObject.ToBson());
         }
+        /// <summary>
+        /// 更新多個IFC物件
+        /// </summary>
+        /// <param name="ifcObjects"></param>
+        /// <returns></returns>
         public int Objects(List<IFCObject> ifcObjects)
         {
             return Database.IFCModel.Update(GetObjects(ifcObjects.ToList<IFCBase>()));
         }
+        /// <summary>
+        /// 更新IFC檔案描述
+        /// </summary>
+        /// <param name="header"></param>
+        /// <returns></returns>
         public bool FileDescription(IFCHeader header)
         {
             if (header.EntityName != "FILE_DESCRIPTION")
@@ -27,6 +49,11 @@ namespace IFCLite.Access
             Database.IFCHead.Update(header.ToBson());
             return true;
         }
+        /// <summary>
+        /// 更新IFC檔案名稱及相關內容
+        /// </summary>
+        /// <param name="header"></param>
+        /// <returns></returns>
         public bool FileName(IFCHeader header)
         {
             if (header.EntityName != "FILE_NAME")
--- src/IFCLiteDrive.cs
+++ src/IFCLiteDrive.cs
@@ -11,20 +11,48 @@ using IFCLite.Access;
 
 namespace IFCLite
 {
+    /// <summary>
+    /// IFCLite主要物件，負責處理所有IFCDatabase事項。
+    /// </summary>
     public class IFCLiteDrive
     {
         private IFCDatabase IFCDatabase { get; set; }
         private SchemaReader SchemaReader { get; set; }
+        /// <summary>
+        /// Basic query module.
+        /// </summary>
         public IFCQuery Query { get { return new IFCQuery(IFCDatabase); } }
+        /// <summary>
+        /// Basic insert module.
+        /// </summary>
         public IFCInsert Insert { get { return new IFCInsert(IFCDatabase); } }
+        /// <summary>
+        /// Basic remove module.
+        /// </summary>
         public IFCDelete Remove { get { return new IFCDelete(IFCDatabase); } }
+        /// <summary>
+        /// Basic update module.
+        /// </summary>
         public IFCUpdate Update { get { return new IFCUpdate(IFCDatabase); } }
+        /// <summary>
+        /// Create new IFCLiteDrive and import Ifc file.
+        /// </summary>
+        /// <param name="dbPath">Path of LiteDB.</param>
+        /// <param name="ifcPath">Path of IFC file.</param>
+        /// <param name="version">Version of IFC.</param>
+        /// <param name="create">If create index.</param>
         public IFCLiteDrive(string dbPath, string ifcPath, IFCVersion version, bool create)
         {
             SchemaReader = new SchemaReader(version);
             IFCDatabase = new IFCDatabase(dbPath,create);
             AddDataToDatabase(ifcPath);
         }
+        /// <summary>
+        /// Create new IFCLiteDrive.
+        /// </summary>
+        /// <param name="dbPath">Path of LiteDB.</param>
+        /// <param name="version">Version of IFC file.</param>
+        /// <param name="create">If create index.</param>
         public IFCLiteDrive(string dbPath, IFCVersion version, bool create)
         {
             SchemaReader = new SchemaReader(version);
@@ -33,7 +61,7 @@ namespace IFCLite
         private void AddDataToDatabase(string ifcpath)
         {
             IFCReader reader = new IFCReader(ifcpath, SchemaReader);
-            Insert.IFCObjectInsert(reader.InsertObjs);
+            Insert.IFCObject(reader.InsertObjs);
             Insert.IFCHeadInsert(reader.Header);
             IFCDatabase.ReplaceTable.Insert(reader.ReplaceTable);
             IFCDatabase.InverseTable.Insert(reader.InverseTable);
@@ -43,10 +71,18 @@ namespace IFCLite
             foreach (IFCBase data in objs)
                 yield return data.ToBson();
         }
+        /// <summary>
+        /// Export IFC file from database.
+        /// </summary>
+        /// <param name="folderPath">Folder path.</param>
+        /// <param name="fileName">File name.</param>
         public void Export(string folderPath, string fileName)
         {
             IFCExport export = new IFCExport(folderPath, fileName, IFCDatabase);
         }
+        /// <summary>
+        /// Database disconnect.
+        /// </summary>
         public void Close()
         {
             IFCDatabase.Close();
